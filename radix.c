46f752940acade58c619719641a70e42b59f837e
gabrielpereirapinheiro <140020764@aluno.unb.br>
2017-05-25 16:17:30 -0300

    First commit

---
+++ radix.c
@@ -0,0 +1,26 @@
+/*PROJETO E ANALISE DE ALGORITMOS 2017-1
+ *ALUNO:GABRIEL PEREIRA PINHEIRO
+ *MATRICULA: 14/0020764
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+
+void show_time(time_t inicio,time_t fim)
+{
+	printf("O tempo de execucao em segundos é %f\n", difftime(fim, inicio));
+}
+
+int main()
+{
+
+	time_t inicio, fim;
+	inicio = time(NULL);
+	sleep(2);
+	fim = time(NULL);
+	show_time(inicio,fim);
+	return 0;
+}

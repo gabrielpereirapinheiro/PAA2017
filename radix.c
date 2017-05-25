b4d213649702137c9c18cdb9f75936ef2ead3a3f
gabrielpereirapinheiro <140020764@aluno.unb.br>
2017-05-25 16:32:05 -0300

    Funcao marca tempo

--- radix.c
+++ radix.c
@@ -8,19 +8,26 @@
 #include <time.h>
 #include <unistd.h>
 
-
+/*Funcao que mostra o tempo de execucao total do programa
+ *Recebe: o tempo inicio e o tempo final do programa
+ *Retorna: 
+*/
 void show_time(time_t inicio,time_t fim)
-{
+{	
 	printf("O tempo de execucao em segundos é %f\n", difftime(fim, inicio));
 }
 
 int main()
 {
-
+	//Variaveis que irao marcar o tempo decorrido
 	time_t inicio, fim;
+
 	inicio = time(NULL);
+
 	sleep(2);
+
 	fim = time(NULL);
+
 	show_time(inicio,fim);
 	return 0;
 }
